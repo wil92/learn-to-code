c4365c55d7724c5da59c1d03b2edfd91283ba65c
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-05-21 16:22:31 +0200

    Add eval microservice in a different docker container

--- start-app.sh
+++ start-app.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 nginx -g 'pid /tmp/nginx.pid;daemon off;' &
-node /app/dist/apps/eval/main.js &
 node /app/dist/apps/api/main.js
---
+++ start-eval.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node /app/dist/apps/eval/main.js
